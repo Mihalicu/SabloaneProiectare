abf9a75bcd530f1e16314d7a5bac3b0b6926ec6f
mihalicinfo@gmail.com <mihalicinfo@gmail.com>
2020-10-27 13:23:38 +0200

    Update ImageProxy.java

--- src/ImageProxy.java
+++ src/ImageProxy.java
@@ -1,16 +1,16 @@
 public class ImageProxy implements Element{
 
     private String name;
-    Image realImage=null;
+    Image realSubject=null;
 
     ImageProxy(String name){
         this.name=name;
     }
 
     public void print() {
-        if(realImage==null){
-            realImage = new Image(this.name);
+        if(realSubject==null){
+            realSubject= new Image(this.name);
         }
-        realImage.print();
+        realSubject.print();
     }
 }
